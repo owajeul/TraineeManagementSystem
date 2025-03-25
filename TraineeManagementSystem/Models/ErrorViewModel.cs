6cdbb46d3b687a4431bcc623ef18edb3eee078e5
Owajeul Islam <owajeul.islam@brainstation-23.com>
2025-03-25 15:35:11 +0600

    Add project files.

---
+++ TraineeManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TraineeManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
