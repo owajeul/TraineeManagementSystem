4e550de4daed1bc80aee824518496d44d400450c
Owajeul Islam <owajeul.islam@brainstation-23.com>
2025-03-25 15:42:35 +0600

    Add trainee model class

---
+++ TraineeManagementSystem/Models/Trainee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraineeManagementSystem.Models
+{
+    public class Trainee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string University { get; set; } = string.Empty;
+
+        [Required]
+        public string Technology { get; set; } = string.Empty ;
+
+        [Required, Phone]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+    }
+
+}
